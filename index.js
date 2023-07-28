2330ce65a3901690e158cf73140fc907305fb04b
my6056 <129304542+my6056@users.noreply.github.com>
2023-07-28 05:25:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const helmet = require("helmet");
 const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
